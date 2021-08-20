e2ab46a6491ea95182ab2a57375e478c14d46d7c
arman-rn <Armawn.rn@gmail.com>
2021-08-20 14:54:43 +0430

    ...

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyDhaLwpYLx15k-gZycxbBSc28rlN7nfyqE';
+const KEY = 'API-KEY';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
